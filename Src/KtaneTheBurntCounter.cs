cfa7df5716736b9b3eddae78741300b2d7f8fc36
Timwi <github@timwi.de>
2026-03-13 13:50:16 +0100

    Upgrade to .NET 10.0

--- Src/KtaneTheBurntCounter.cs
+++ Src/KtaneTheBurntCounter.cs
@@ -1,16 +1,6 @@
-﻿using System.Reflection;
-using System.Runtime.InteropServices;
-using RT.Json;
+﻿using RT.Json;
 using RT.PropellerApi;
 using RT.Servers;
-
-[assembly: AssemblyTitle("KtaneTheBurntCounter")]
-[assembly: AssemblyProduct("KtaneTheBurntCounter")]
-[assembly: AssemblyCopyright("Copyright © BakersDozenBagels 2021")]
-[assembly: ComVisible(false)]
-[assembly: Guid("48f717f6-ab9f-4dff-9796-218cce1311b3")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
 
 namespace KtaneTheBurnt
 {
